aff8641bb37c6530a8d4e21e90c2749ee0afb667
Buyanzaya12 <157042363+Buyanzaya12@users.noreply.github.com>
2024-07-03 17:59:57 +0800

    Add files via upload

---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+import { HiSun } from "react-icons/hi";
+export function Header() {
+    return <div class="">
+        <nav class="flex items-center h-16 justify-end gap-x-3.5 mr-12" >
+            <div className="">About</div>
+            <div className="">Work</div>
+            <div className="">Contact</div>
+            <div class="ml-2"><HiSun /></div>
+            <div class=" bg-white text-black p-1 rounded-lg">Download CV</div>
+        </nav>
+    </div>;
+}
